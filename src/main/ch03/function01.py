598db71a88d3f986053d988c1db964e6038ed413
NPsky-des <jhl9099@naver.com>
2025-04-23 17:39:38 +0900

    feat: 20250423 python dict set if for fuction

--- src/main/ch03/function01.py
+++ src/main/ch03/function01.py
@@ -3,10 +3,10 @@ result = 10 + 20
 def get10():
     return 5 + 3 + 2
 
-result = get10() + 20
-                    2
-20                  +
-+                   3
-get10()     ->      +
-=                   5
-result              return
+# result = get10() + 20
+#                     2
+# 20                  +
+# +                   3
+# get10()     ->      +
+# =                   5
+# result              return
